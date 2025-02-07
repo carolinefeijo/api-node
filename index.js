e50a45d782aab5f65efcd3f0baeb1f0c25f27c4a
Caroline Feijo <contatocarolinefeijo@gmail.com>
2025-02-07 07:49:37 -0300

    ajustes de cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Bem vindo!");
